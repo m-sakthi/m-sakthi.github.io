a31aeeeb6b04c0f40d1c17921ca3e52a3212eea5
Sakthivel M <msv300@gmail.com>
2023-01-06 20:24:00 +0100

    Fix: Added styles for mobile view

--- assets/js/globe.js
+++ assets/js/globe.js
@@ -89,26 +89,26 @@ group.add(atmosphere);
 scene.add(group);
 
 // Starts
-function generateStarts(front = false) {
-  const starGeometry = new BufferGeometry();
-  const starMaterial = new PointsMaterial({
-    color: 0xffffff,
-  });
+// function generateStarts(front = false) {
+//   const starGeometry = new BufferGeometry();
+//   const starMaterial = new PointsMaterial({
+//     color: 0xffffff,
+//   });
 
-  const starVertices = [];
-  for (let i = 0; i < 10000; i++) {
-    const x = (Math.random() - 0.5) * 2000;
-    const y = (Math.random() - 0.5) * 2000;
-    const z = Math.random() * 2000;
-    starVertices.push(x, y, front ? z : -z);
-  }
+//   const starVertices = [];
+//   for (let i = 0; i < 10000; i++) {
+//     const x = (Math.random() - 0.5) * 2000;
+//     const y = (Math.random() - 0.5) * 2000;
+//     const z = Math.random() * 2000;
+//     starVertices.push(x, y, front ? z : -z);
+//   }
 
-  starGeometry.setAttribute('position', new Float32BufferAttribute(starVertices, 3))
+//   starGeometry.setAttribute('position', new Float32BufferAttribute(starVertices, 3))
 
-  return new Points(starGeometry, starMaterial);
-}
+//   return new Points(starGeometry, starMaterial);
+// }
 
-const stars = generateStarts();
+// const stars = generateStarts();
 // const starsFront = generateStarts(true);
 
 // scene.add(stars);
